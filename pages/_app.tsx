3d4662c9655f248a0830c6e8bdc3f30aee4330a8
Rohit <rm831799@gmail.com>
2022-02-11 23:36:58 +0530

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
+  <Head>
+    <script src="https://cdn.tailwindcss.com"></script>
+  </Head>
   return <Component {...pageProps} />
 }
 
