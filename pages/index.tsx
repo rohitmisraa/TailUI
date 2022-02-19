f8030e7088d6d98641e54beca3d49d33f79258b4
Rohit <rm831799@gmail.com>
2022-02-20 00:29:35 +0530

    All errors fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,10 @@
 import Head from 'next/head'
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import Image from 'next/image'
 import Nav from '../components/layouts/nav'
-import Drawer from '../components/layouts/Drawer'
-import Stamps from '../components/layouts/Stamps'
-// import styles from '../styles/Home.module.css'
-import SVG from "../svg"
 import Link from 'next/link'
 import Footer from '../components/layouts/Footer'
 
 
-export default function Home({posts: {postdoc, postcomp}}) {
-  // console.log(postdoc);
-  // console.log(postcomp);
+export default function Home() {
   
   
   return (
@@ -38,57 +28,3 @@ export default function Home({posts: {postdoc, postcomp}}) {
   )
 }
     
-export async function getStaticProps() {
-  const filesdoc = fs.readdirSync(path.join('posts/documentation'))
-  const filescompo = fs.readdirSync(path.join('posts/components'))
-
-  const postdoc = filesdoc.map((__filename) => {
-    const slug = __filename.replace('.md', '')
-
-    const MarkdownDatadoc = fs.readFileSync(
-      path.join('posts/documentation', __filename),
-      'utf-8'
-    )
-    
-    
-    
-      
-      const { data:documents } = matter(MarkdownDatadoc);
-    
-
-    return{
-      slug,
-      documents,
-    }
-  })
-  const postcomp = filescompo.map((__filename) => {
-    const slug2 = __filename.replace('.md', '')
-
-    const MarkdownDatacomp = fs.readFileSync(
-      path.join('posts/components', __filename),
-      'utf-8'
-    )
-    
-    
-    
-      
-      const { data:components } = matter(MarkdownDatacomp);
-    
-
-    return{
-      slug2,
-      components,
-    }
-  })
-  
-  
-  return {
-    props: {
-      posts: {
-        postdoc,
-        postcomp
-
-      }
-    }
-  }
-};
