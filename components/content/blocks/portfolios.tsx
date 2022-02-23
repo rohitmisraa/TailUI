a61d68c3a6759ff4f707ee6f2e4f1478a53db4e4
Rohit <rm831799@gmail.com>
2022-02-24 01:25:15 +0530

    Stamp error fixed

--- components/content/blocks/portfolios.tsx
+++ components/content/blocks/portfolios.tsx
@@ -165,26 +165,26 @@ export default function Portfolios() {
                                         &lt;span className=&quot;text-xl text-cl-red&quot;&gt;<br />01. TailUI&lt;/span&gt;<br />
                                         &lt;div className=&quot;h-1 rounded-2xl bg-gradient-to-r from-gr_green-1 to-gr_green-2 w-32 relative top-3 ml-5&quot;&gt;<br />&lt;/div&gt;<br />
                                     &lt;/div&gt;<br />
-                                    &lt;div className='md:h-[400px] xm:mx-10 w-full md:bg-transparent bg-cl-black-carbon m-auto p-2 rounded-2xl overflow-y-hidden'&gt;<br />
-                                        &lt;div className='h-96 w-full md:w-[500px] rounded-[40px] overflow-hidden'&gt;<br />
-                                            &lt;img className='h-full object-cover object-left'
+                                    &lt;div className=&apos;md:h-[400px] xm:mx-10 w-full md:bg-transparent bg-cl-black-carbon m-auto p-2 rounded-2xl overflow-y-hidden&apos;&gt;<br />
+                                        &lt;div className=&apos;h-96 w-full md:w-[500px] rounded-[40px] overflow-hidden&apos;&gt;<br />
+                                            &lt;img className=&apos;h-full object-cover object-left&apos;
                                                 src=&quot;/assets/posts/bg-hero-tailui.jpg&quot; alt=&quot;Tumbnail Image&quot; /&gt;<br />
 
                                         &lt;/div&gt;<br />
-                                        &lt;div className='md:float-right w-full md:p-0 md:w-[600px] md:relative md:bottom-96 text-white'&gt;<br />
-                                            &lt;h3 className='text-3xl sm:text-4xl md:float-right md:relative md:top-5 m-5 md:m-0 md:right-24 text-w md:text-white'&gt;<br />TailUI&lt;/h3&gt;<br />
-                                            &lt;div className='md:mt-16 lg:mt-8 py-5 md:float-right px-5 md:px-16 w-full md:w-3/4 lg:w-auto md:relative left-20 lg:left-0 bg-cl-black-carbon md:rounded-tr-[100px] md:rounded-bl-[100px] sm:shadow-xl'&gt;<br />
-                                                &lt;p className='text-cl-secondary text-base sm:text-xl md:text-base'&gt;<br />TailUI is a Tailwind components library, It has 50+ components and many blocks available.TailUI is a Tailwind components library, It has 50+ components and many blocks available.TailUI is a Tailwind components library, It has 50+ components and many blocks available.TailUI is a Tailwind components library, It has 50+ components and many blocks available.&lt;/p&gt;<br />
+                                        &lt;div className=&apos;md:float-right w-full md:p-0 md:w-[600px] md:relative md:bottom-96 text-white&apos;&gt;<br />
+                                            &lt;h3 className=&apos;text-3xl sm:text-4xl md:float-right md:relative md:top-5 m-5 md:m-0 md:right-24 text-w md:text-white&apos;&gt;<br />TailUI&lt;/h3&gt;<br />
+                                            &lt;div className=&apos;md:mt-16 lg:mt-8 py-5 md:float-right px-5 md:px-16 w-full md:w-3/4 lg:w-auto md:relative left-20 lg:left-0 bg-cl-black-carbon md:rounded-tr-[100px] md:rounded-bl-[100px] sm:shadow-xl&apos;&gt;<br />
+                                                &lt;p className=&apos;text-cl-secondary text-base sm:text-xl md:text-base&apos;&gt;<br />TailUI is a Tailwind components library, It has 50+ components and many blocks available.TailUI is a Tailwind components library, It has 50+ components and many blocks available.TailUI is a Tailwind components library, It has 50+ components and many blocks available.TailUI is a Tailwind components library, It has 50+ components and many blocks available.&lt;/p&gt;<br />
                                             &lt;/div&gt;<br />
-                                            &lt;div className='flex md:float-right mt-2 mx-5  md:text-white'&gt;<br />
+                                            &lt;div className=&apos;flex md:float-right mt-2 mx-5  md:text-white&apos;&gt;<br />
                                                 &lt;Link href=&quot;https://tailui.xyz&quot; passHref&gt;<br />&lt;a target=&quot;_blank&quot;&gt;<br />
-                                                    &lt;button className='mx-5'&gt;<br />
+                                                    &lt;button className=&apos;mx-5&apos;&gt;<br />
                                                         &lt;svg viewBox=&quot;0 0 16 16&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot; width=&quot;35&quot; height=&quot;35&quot; fill=&quot;currentColor&quot;&gt;<br />
                                                             &lt;path fillRule=&quot;evenodd&quot; d=&quot;M5,2 C5.55228,2 6,2.44772 6,3 C6,3.55228 5.55228,4 5,4 L4,4 L4,12 L12,12 L12,11 C12,10.4477 12.4477,10 13,10 C13.5523,10 14,10.4477 14,11 L14,12 C14,13.1046 13.1046,14 12,14 L4,14 C2.89543,14 2,13.1046 2,12 L2,4 C2,2.89543 2.89543,2 4,2 L5,2 Z M15,1 L15,5.99814453 C15,6.55043453 14.5523,6.99814453 14,6.99814453 C13.4477,6.99814453 13,6.55043453 13,5.99814453 L13,4.41419 L8.71571,8.69846 C8.32519,9.08899 7.69202,9.08899 7.3015,8.69846 C6.91097,8.30794 6.91097,7.67477 7.3015,7.28425 L11.5858,3 L9.99619141,3 C9.44391141,3 8.99619141,2.55228 8.99619141,2 C8.99619141,1.44772 9.44391141,1 9.99619141,1 L15,1 Z&quot; /&gt;<br />
                                                         &lt;/svg&gt;<br />
                                                     &lt;/button&gt;<br />&lt;/a&gt;<br />&lt;/Link&gt;<br />
                                                 &lt;Link href=&quot;https://github.com/rohitmra/tailui&quot; passHref&gt;<br />&lt;a target=&quot;_blank&quot;&gt;<br />
-                                                    &lt;button className='mx-5'&gt;<br />
+                                                    &lt;button className=&apos;mx-5&apos;&gt;<br />
                                                         &lt;svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 496 512&quot; width=&quot;35&quot; height=&quot;35&quot; fill=&quot;currentColor&quot;&gt;<br />
                                                             &lt;path d=&quot;M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z&quot; /&gt;<br />&lt;/svg&gt;<br />
 
@@ -204,7 +204,7 @@ export default function Portfolios() {
                                     &lt;/a&gt;<br />&lt;/Link&gt;<br />
                                     &lt;h4 className=&quot;mt-40 mb-3 text-2xl text-cl-secondary flex w-max m-auto space-x-1&quot;&gt;<br />
                                         &lt;span&gt;<br />Made with&lt;/span&gt;<br />
-                                        &lt;svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 512 512&quot; fill='#EF233C' width=&quot;30&quot; height=&quot;30&quot;&gt;<br />&lt;path d=&quot;M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z&quot; /&gt;<br />&lt;/svg&gt;<br />
+                                        &lt;svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 512 512&quot; fill=&apos;#EF233C&apos; width=&quot;30&quot; height=&quot;30&quot;&gt;<br />&lt;path d=&quot;M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z&quot; /&gt;<br />&lt;/svg&gt;<br />
                                         &lt;span&gt;<br />using &lt;Link href=&quot;https://tailui.xyz&quot; passHref&gt;<br />&lt;a className=&quot;text-cl-red&quot; target=&quot;_blank&quot;&gt;<br />TailUI&lt;/a&gt;<br />&lt;/Link&gt;<br />&lt;/span&gt;<br />
                                     &lt;/h4&gt;<br />
                                 &lt;/div&gt;<br />
--- components/layouts/Stamps.tsx
+++ components/layouts/Stamps.tsx
@@ -7,7 +7,12 @@ export default function Stamps({list}) {
     <div className='hidden xl:block w-[300px] h-full pt-8 xl:pl-10 fixed justify-end'>
       <h1 className='text-skin-dark text-xl font-medium relative right-4'>Contents</h1>
           <ul className=' text-base font-Rubik text-skin-light'>
-              {list.map(stamp => <li className='my-1'><Link href={"#"+stamp.code} passHref>{stamp.title}</Link></li>)}
+              {list.map(stamp =>
+                 <li className='my-1' key={stamp.id}>
+                   <Link href={"#"+stamp.code} passHref>
+                     {stamp.title}
+                   </Link>
+                 </li>)}
           </ul>
         </div>
   )
